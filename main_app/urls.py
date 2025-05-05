0f7e561c825f6b55a8d22837c93893f898e1b8b5
GurtejB-43 <gbagga@purdue.edu>
2025-05-05 00:49:55 -0400

    added the updated path for the filtered report to urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('expense/delete/<int:pk>/', views.delete_expense, name='delete_expense'),
     path('expense/edit/<int:pk>/', views.edit_expense, name='edit_expense'),
-    path('expense/report/', views.report, name='report'),
-    path('expense/category_report/', views.category_report, name='category_report'),
-    path('expense/date_report/', views.date_report, name='date_report')
+    path('expense/report/', views.filtered_report, name='report'),
 ]
